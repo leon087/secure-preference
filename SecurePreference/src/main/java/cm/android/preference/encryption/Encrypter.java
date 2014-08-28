40e407fd878a79ed71429f8556261af08413adba
leon087 <leo.zhuge@gmail.com>
2014-08-28 23:00:25 +0800

    新增签名数据

--- SecurePreference/src/main/java/cm/android/preference/encryption/Encrypter.java
+++ SecurePreference/src/main/java/cm/android/preference/encryption/Encrypter.java
@@ -59,12 +59,7 @@ public class Encrypter implements IEncrypt {
     public static class KeyHelper {
 
         public static byte[] initIv(Context context, String tag, SharedPreferences preference) {
-            String sigStr = "";
-            android.content.pm.Signature[] signatures = Util.getSignature(context.getPackageManager(), context.getPackageName());
-            if (signatures != null && signatures.length > 0) {
-                sigStr = signatures[0].toCharsString();
-            }
-            char[] password = (context.getPackageName() + tag + sigStr).toCharArray();
+            char[] password = getPassword(context, tag).toCharArray();
             final byte[] salt = getDeviceSerialNumber(context).getBytes();//
 
             try {
@@ -86,8 +81,17 @@ public class Encrypter implements IEncrypt {
             }
         }
 
+        private static String getPassword(Context context, String tag) {
+            String sigStr = "";
+            android.content.pm.Signature[] signatures = Util.getSignature(context.getPackageManager(), context.getPackageName());
+            if (signatures != null && signatures.length > 0) {
+                sigStr = signatures[0].toCharsString();
+            }
+            return context.getPackageName() + tag + sigStr;
+        }
+
         public static byte[] initKey(Context context, String tag, SharedPreferences preference) {
-            final byte[] seed = (context.getPackageName() + tag).getBytes();
+            final byte[] seed = getPassword(context, tag).getBytes();
 
             try {
                 Key key = AESCoder.generateKey(seed);
