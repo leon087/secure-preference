eb8d3febd1aa4c345aecc23fcfb693182e9ca834
leon087 <leo.zhuge@gmail.com>
2014-09-01 21:15:05 +0800

    更新AES加密

--- SecurePreference/src/main/java/cm/android/preference/util/AESCoder.java
+++ SecurePreference/src/main/java/cm/android/preference/util/AESCoder.java
@@ -7,24 +7,13 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
 
 public class AESCoder {
     private static final int KEY_SIZE = 256;
 
     public static final String KEY_ALGORITHM = "AES";
     public static final String C_AES_CBC_PKCS5PADDING = "AES/CBC/PKCS5Padding";
-
-    public static byte[] encryptBySeed(byte[] seed, byte[] iv, byte[] data) throws Exception {
-        byte[] rawKey = generateKey(seed).getEncoded();
-        byte[] result = encrypt(rawKey, iv, data);
-        return result;
-    }
-
-    public static byte[] decryptBySeed(byte[] seed, byte[] iv, byte[] encrypted) throws Exception {
-        byte[] rawKey = generateKey(seed).getEncoded();
-        byte[] result = decrypt(rawKey, iv, encrypted);
-        return result;
-    }
 
     public static SecretKey generateKey(byte[] seed) throws Exception {
         KeyGenerator kgen = KeyGenerator.getInstance(KEY_ALGORITHM);
@@ -35,25 +24,6 @@ public class AESCoder {
         return secretKey;
     }
 
-    public static byte[] encrypt(byte[] key, byte[] iv, byte[] src) throws Exception {
-        SecretKeySpec skeySpec = new SecretKeySpec(key, C_AES_CBC_PKCS5PADDING);
-        IvParameterSpec ivSpec = SecureUtil.getIv(iv);
-
-        Cipher cipher = Cipher.getInstance(C_AES_CBC_PKCS5PADDING);
-        cipher.init(Cipher.ENCRYPT_MODE, skeySpec, ivSpec);
-        byte[] encrypted = cipher.doFinal(src);
-        return encrypted;
-    }
-
-    public static byte[] decrypt(byte[] key, byte[] iv, byte[] encrypted) throws Exception {
-        SecretKeySpec skeySpec = new SecretKeySpec(key, C_AES_CBC_PKCS5PADDING);
-        IvParameterSpec ivSpec = SecureUtil.getIv(iv);
-
-        Cipher cipher = Cipher.getInstance(C_AES_CBC_PKCS5PADDING);
-        cipher.init(Cipher.DECRYPT_MODE, skeySpec, ivSpec);
-        byte[] decrypted = cipher.doFinal(encrypted);
-        return decrypted;
-    }
 
     public static SecretKey generateKey() throws NoSuchAlgorithmException {
         // Do *not* seed secureRandom! Automatically seeded from system entropy
@@ -74,4 +44,51 @@ public class AESCoder {
         return generator.generateKey();
         //return SecureUtil.encode(generator.generateKey().getEncoded());
     }
+
+    public static SecretKey generateKey(char[] password, byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        SecretKey tmp = HashUtil.generateHash(password, salt);
+        SecretKey secret = new SecretKeySpec(tmp.getEncoded(), C_AES_CBC_PKCS5PADDING);
+        return secret;
+    }
+
+    public static byte[] encryptBySeed(byte[] seed, byte[] iv, byte[] data) throws Exception {
+        byte[] rawKey = generateKey(seed).getEncoded();
+        byte[] result = encrypt(rawKey, iv, data);
+        return result;
+    }
+
+    public static byte[] decryptBySeed(byte[] seed, byte[] iv, byte[] encrypted) throws Exception {
+        byte[] rawKey = generateKey(seed).getEncoded();
+        byte[] result = decrypt(rawKey, iv, encrypted);
+        return result;
+    }
+
+    public static byte[] encrypt(byte[] key, byte[] iv, byte[] src) throws Exception {
+        SecretKeySpec skeySpec = new SecretKeySpec(key, C_AES_CBC_PKCS5PADDING);
+        return encrypt(skeySpec, iv, src);
+    }
+
+    public static byte[] decrypt(byte[] key, byte[] iv, byte[] encrypted) throws Exception {
+        SecretKeySpec skeySpec = new SecretKeySpec(key, C_AES_CBC_PKCS5PADDING);
+        return decrypt(skeySpec, iv, encrypted);
+    }
+
+    public static byte[] encrypt(SecretKey secretKey, byte[] iv, byte[] src) throws Exception {
+        IvParameterSpec ivSpec = SecureUtil.getIv(iv);
+
+        Cipher cipher = Cipher.getInstance(C_AES_CBC_PKCS5PADDING);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivSpec);
+        byte[] encrypted = cipher.doFinal(src);
+        return encrypted;
+    }
+
+    public static byte[] decrypt(SecretKey secretKey, byte[] iv, byte[] encrypted) throws Exception {
+        IvParameterSpec ivSpec = SecureUtil.getIv(iv);
+
+        Cipher cipher = Cipher.getInstance(C_AES_CBC_PKCS5PADDING);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, ivSpec);
+        byte[] decrypted = cipher.doFinal(encrypted);
+        return decrypted;
+    }
+
 }
